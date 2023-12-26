69bb5954372280b3147c4e932d720ab4b5e34cd6
Avag Sargsyan <avagsargsyan88@gmail.com>
2023-12-26 16:09:04 +0400

    tidy up

--- main.go
+++ main.go
@@ -14,42 +14,6 @@ import (
 	"time"
 )
 
-type Service func(ctx context.Context) (int64, error)
-
-//func main() {
-//	svcs := []Service{
-//		Service1,
-//		Service2,
-//		Service3,
-//	}
-//
-//	var total int64
-//
-//	start := time.Now()
-//
-//	g, ctx := errgroup.WithContext(context.Background())
-//
-//	for _, svc := range svcs {
-//		svc := svc
-//		g.Go(func() error {
-//			v, err := svc(ctx)
-//			if err != nil {
-//				return err
-//			}
-//
-//			_ = atomic.AddInt64(&total, v)
-//
-//			return nil
-//		})
-//	}
-//
-//	if err := g.Wait(); err != nil {
-//		log.Fatalln("Group", err)
-//	}
-//
-//	fmt.Println("Total", total, "Duration", time.Now().Sub(start))
-//}
-
 var (
 	logger           = watermill.NewStdLogger(false, false)
 	googleCloudTopic = "events"
@@ -65,25 +29,35 @@ type MyEvent struct {
 }
 
 func main() {
+	// init mongodb connection
 	initMongoClient("mongodb://mongodb:27017")
+
+	// create a new watermill message router
 	router, err := message.NewRouter(message.RouterConfig{}, logger)
 	if err != nil {
 		log.Fatalln("Router", err)
 	}
 
+	// this adds a plugin to the router for handling OS signals
 	router.AddPlugin(plugin.SignalsHandler)
+
+	// middleware for automatic recovery
 	router.AddMiddleware(middleware.Recoverer)
 
+	// get a publisher and subscriber
 	publisher, subscriber := setupPubSub()
+
+	// generate and publish events continuously without blocking the main thread
 	go simulateEvents()
 
+	// add a handler to the router subscribed to googleCloudTopic and process incoming events
 	router.AddHandler(
-		handlerName,
-		googleCloudTopic,
-		subscriber,
-		"dummy_topic",
-		publisher,
-		func(msg *message.Message) ([]*message.Message, error) {
+		handlerName,      // handler name
+		googleCloudTopic, // topic to subscribe to
+		subscriber,       // subscriber which receives messages from the topic
+		"dummy_topic",    // topic to publish to, not used here
+		publisher,        // publisher which publishes messages to the topic
+		func(msg *message.Message) ([]*message.Message, error) { // process incoming messages
 			consumedEvent := MyEvent{}
 			err := json.Unmarshal(msg.Payload, &consumedEvent)
 			if err != nil {
@@ -92,24 +66,31 @@ func main() {
 
 			log.Printf("received event %+v with UUID %s", consumedEvent, msg.UUID)
 
+			// return indicated that the message has been processed successfully
+			// in this case handler doesn't produce any new messages to be published
 			return []*message.Message{msg}, nil
 		},
 	)
 
+	// start the router to process incoming events
 	if err := router.Run(context.Background()); err != nil {
-		panic(err)
+		log.Fatalf("could not start router: %v", err)
 	}
 }
 
+// simulateEvents continuously generates and publishes events to a Google Cloud Pub/Sub topic
 func simulateEvents() {
+	// Initialize a new publisher for Google Cloud Pub/Sub
 	pub, err := googlecloud.NewPublisher(googlecloud.PublisherConfig{}, logger)
 	if err != nil {
-		panic(err)
+		log.Fatalf("could not create publisher: %v", err)
 	}
 
 	log.Printf("publishing events to topic %s", googleCloudTopic)
 
+	// Init a loop to continuously generate and publish events
 	for {
+		// Generate a new event with unique ID
 		e := MyEvent{
 			ID:        watermill.NewUUID(),
 			EventType: "example_event",
@@ -119,9 +100,10 @@ func simulateEvents() {
 
 		log.Printf("publishing event %+v", e)
 
+		// Marshal event to JSON
 		payload, err := json.Marshal(e)
 		if err != nil {
-			panic(err)
+			log.Fatalf("could not marshal event: %v", err)
 		}
 
 		// Store event in MongoDB, should be donne in a transaction
@@ -133,33 +115,20 @@ func simulateEvents() {
 			log.Println("Event stored:", e)
 		}
 
+		// Publish event to Google Cloud Pub/Sub
 		err = pub.Publish(googleCloudTopic, message.NewMessage(
 			watermill.NewUUID(),
 			payload,
 		))
 		if err != nil {
-			panic(err)
+			log.Fatalf("could not publish event: %v", err)
 		}
 
 		time.Sleep(5 * time.Second)
 	}
 }
 
-func Service1(ctx context.Context) (int64, error) {
-	time.Sleep(100 * time.Millisecond)
-	return 1, nil
-}
-
-func Service2(ctx context.Context) (int64, error) {
-	time.Sleep(100 * time.Millisecond)
-	return 2, nil
-}
-
-func Service3(ctx context.Context) (int64, error) {
-	time.Sleep(200 * time.Millisecond)
-	return 3, nil
-}
-
+// initMongoClient initializes a MongoDB client
 func initMongoClient(uri string) {
 	var err error
 	mongoClient, err = mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
@@ -168,15 +137,21 @@ func initMongoClient(uri string) {
 	}
 }
 
+// storeEventInMongo stores an event in MongoDB
 func storeEventInMongo(event MyEvent) error {
 	collection := mongoClient.Database("testdb").Collection("events")
 	_, err := collection.InsertOne(context.Background(), event)
 	return err
 }
 
+// setupPubSub sets up a Google Cloud Pub/Sub publisher and subscriber
+// implementation details: https://watermill.io/pubsubs/googlecloud/
 func setupPubSub() (message.Publisher, message.Subscriber) {
+	// init watermill logger
 	logger := watermill.NewStdLogger(false, false)
 
+	// create a new publisher
+	// we need this to create a handler
 	publisher, err := googlecloud.NewPublisher(
 		googlecloud.PublisherConfig{},
 		logger,
@@ -185,6 +160,8 @@ func setupPubSub() (message.Publisher, message.Subscriber) {
 		log.Fatalf("could not create publisher: %v", err)
 	}
 
+	// create a new subscriber
+	// we need this to create a handler
 	subscriber, err := googlecloud.NewSubscriber(
 		googlecloud.SubscriberConfig{
 			GenerateSubscriptionName: func(topic string) string {
